d1d4d648be56144cb174763ce9c4a676a177cd60
kheyPha <markriispapp@hotmail.com>
2020-06-03 18:40:29 +0300

    v1

--- Form1.cs
+++ Form1.cs
@@ -17,7 +17,9 @@ namespace TrafficLight
         {
             InitializeComponent();
             InitializeLights();
-            RoundedEdges();
+            InitializeTSwitch();
+
+            RoundedEdges(); // Starts the edge rounding stuff
         }
 
         private void InitializeLights()
@@ -27,11 +29,31 @@ namespace TrafficLight
             ColorGrn.BackColor = Color.Gray;
         }
 
+        private void InitializeTSwitch()
+        {
+            timerSwitch = new Timer();
+            timerSwitch.Interval = 1000;
+            timerSwitch.Tick += new EventHandler(TimerSwitch_Tick);
+            timerSwitch.Start();
+        }
+
+        private void TimerSwitch_Tick(object sender, EventArgs e)
+        {
+            if(ColorYlw.BackColor == Color.Gray)
+            {
+                ColorYlw.BackColor = Color.DarkOrange;
+            }
+            else
+            {
+                ColorYlw.BackColor = Color.Gray;
+            }
+        }
+
         private void RoundedEdges() // This is SO messy, might try making it clearer l8r
         {
             Rectangle r = new Rectangle(0, 0, ColorRed.Width, ColorRed.Height);
             System.Drawing.Drawing2D.GraphicsPath rp = new System.Drawing.Drawing2D.GraphicsPath();
-            int d = 50;
+            int d = 150;
             rp.AddArc(r.X, r.Y, d, d, 180, 90);
             rp.AddArc(r.X + r.Width - d, r.Y, d, d, 270, 90);
             rp.AddArc(r.X + r.Width - d, r.Y + r.Height - d, d, d, 0, 90);
@@ -40,7 +62,7 @@ namespace TrafficLight
 
             Rectangle y = new Rectangle(0, 0, ColorYlw.Width, ColorYlw.Height);
             System.Drawing.Drawing2D.GraphicsPath yp = new System.Drawing.Drawing2D.GraphicsPath();
-            int f = 50;
+            int f = 150;
             yp.AddArc(y.X, y.Y, f, f, 180, 90);
             yp.AddArc(y.X + y.Width - f, y.Y, f, f, 270, 90);
             yp.AddArc(y.X + y.Width - f, y.Y + y.Height - f, f, f, 0, 90);
@@ -49,7 +71,7 @@ namespace TrafficLight
 
             Rectangle g = new Rectangle(0, 0, ColorGrn.Width, ColorGrn.Height);
             System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
-            int e = 50;
+            int e = 150;
             gp.AddArc(g.X, g.Y, e, e, 180, 90);
             gp.AddArc(g.X + g.Width - e, g.Y, e, e, 270, 90);
             gp.AddArc(g.X + g.Width - e, g.Y + g.Height - e, e, e, 0, 90);
